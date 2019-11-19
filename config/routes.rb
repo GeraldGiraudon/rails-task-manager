a030d9f12d2de421072ac85b76b8931e838596ab
gerald giraudon <gerald13006@gmail.com>
2019-11-19 18:00:36 +0100

    tasks manager not finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,28 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+get 'tasks', to: 'tasks#index'
+
+get 'tasks/new', to: 'tasks#new'
+post 'tasks', to: 'tasks#create'
+
+get 'tasks/:id', to: 'tasks#show', as: :task
+
+get 'tasks/:id/edit', to: 'tasks#edit'
+patch 'tasks/:id', to: 'tasks#update'
+
 end
+
+# Rails.application.routes.draw do
+#   get    "restaurants",          to: "restaurants#index"
+
+#   get    "restaurants/new",      to: "restaurants#new"
+#   post   "restaurants",          to: "restaurants#create"
+
+#   # NB: The `show` route needs to be *after* `new` route.
+#   get    "restaurants/:id",      to: "restaurants#show"
+
+#   get    "restaurants/:id/edit", to: "restaurants#edit"
+#   patch  "restaurants/:id",      to: "restaurants#update"
+
+#   delete "restaurants/:id",      to: "restaurants#destroy"
+# end
